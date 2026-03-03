3e8e2c99e5595fa6d7ba7df031add3641864cd56
Ekaterina <ekaterinicheva.e@gmail.com>
2026-03-03 08:00:44 -0500

    Add the total length of the typed text feature

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -39,10 +39,14 @@ function endTest() {
         wpm = Math.round((typedWords / timeElapsed) * 60);
     }
 
+    // Calculate the total length of text the user typed 
+    let textLength = userTypedText.length;
+
     // Display the results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
-        "<p>Words Per Minute (WPM): " + wpm + "</p>";
+        "<p>Words Per Minute (WPM): " + wpm + "</p>"
+        "<p>Total length: " + textLength + "</p>";
     }
